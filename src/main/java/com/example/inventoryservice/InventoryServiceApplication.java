ea027dd8debed96df29872e13aab902ef7880d30
ayman rahmani <rahmani.ayman17@gmail.com>
2022-12-03 23:25:18 +0100

    invontoryService v0.2

--- src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -16,9 +17,10 @@ public class InventoryServiceApplication {
 
 
     @Bean
-    CommandLineRunner start(ProductRepo productRepo)
+    CommandLineRunner start(ProductRepo productRepo, RepositoryRestConfiguration configuration)
     {
         return args -> {
+            configuration.exposeIdsFor(Product.class);
             productRepo.save(new Product(null,"hp laptpo",1526));
             productRepo.save(new Product(null,"dell laptpo",4545));
             productRepo.save(new Product(null,"lenevo laptpo",6548));
